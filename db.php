811e29b43fd4622922647d1d179bcc82c58cd232
heloisaangh <91680274+heloisaangh@users.noreply.github.com>
2022-04-19 09:30:46 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,27 +1,48 @@
-<?php 
-class BD{
+<?php
+class BD
+{
 
-    private $host ="localhost";// ip que está conectando 
-    private $dbname ="db_aula_tai";// nome do bd criado
+    private $host = "localhost"; // ip que está conectando 
+    private $dbname = "db_aula_tai"; // nome do bd criado
     private $port = 3306; // porta que vai se conectar
     private $usuario = "root"; //
     private $senha = "";
     private $db_charset = "utf8";
 
-    public function conn(){// função p/ conexão com bd
+    public function conn()
+    { // função p/ conexão com bd
         $conn = "mysql:host=$this->host;
-            dbname=$this->dbname; port=$this->port";// $conn = string de conexão  
+            dbname=$this->dbname; port=$this->port"; // $conn = string de conexão  
 
-        return new PDO($conn,$this->usuario, $this->senha,
-        [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES ". $this->db_charset]
-    );
+        return new PDO(
+            $conn,
+            $this->usuario,
+            $this->senha,
+            [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . $this->db_charset]
+        );
     }
 
-    public function select(){
+    public function select()
+    {
         $conn = $this->conn();
         $st = $conn->prepare("SELECT * FROM usuario;");
         $st->execute();
         return $st;
     }
+    public function inserir($dados)
+    {
+        $conn = $this->conn();
+        $sql = "INSERT INTO usuario(nome,telefone,cpf) value(?,?,?)";
+        $st = $conn->prepare($sql);
+        $arryDados = []; //receber valores do formulario em forma de array
+        $arryDados[] = $dados['nome'];
+        $arryDados[] = $dados['telefone'];
+        $arryDados[] = $dados['cpf'];
+        $st->execute($arryDados);//passa o vetor 
+
+        return $st;
+    }
+    public function insert()
+    {
+    }
 }
-?>
