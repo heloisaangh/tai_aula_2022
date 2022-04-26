5a1fc124d87d26ab43f1bcf947f47e9482e10fb1
heloisaangh <91680274+heloisaangh@users.noreply.github.com>
2022-04-26 09:30:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php
-include "./database/db.php" ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,35 +9,8 @@ include "./database/db.php" ?>
 </head>
 
 <body>
-
-    <br>
-    <?php
-    $objBD = new BD();
-    $objBD->conn(); //abre a conexão
-    $result = $objBD->select(); // retorna um array com os dados do bd
-    foreach ($result as $item) {
-        echo "ID:" . $item['id'] . " Nome: " . $item['nome'] . "<br>";
-    }
-
-    if (!empty($_POST)) {
-        echo "Salvar<br>";
-        var_dump($_POST);
-        $objBD->inserir($_POST);
-        header("Location:index.php");
-    }
-
-    ?>
-
-<h2>Formulário Cliente</h2>
-    <form action="index.php" method="post">
-        <label for="nome">Nome:</label></br>
-        <input type="text" id="id" name="nome"></br>
-        <label>Telefone:</label></br>
-        <input type="text" id="id1" name="telefone" placeholder="(49)99999-9999"></br>
-        <lablel for="cpf">CPF:</lablel></br>
-        <input type="text" id="id2" name="cpf" placeholder="000.000.000.00"><br></br>
-        <input type="submit" value="Salvar">
-    </form>
+<h2>CRUD</h2>
+<a href="pagina/usuarioList.php">Ver</a>
 </body>
 
 </html>
