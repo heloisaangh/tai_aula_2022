44b2785c204f8efeff643ff3b165d9e08350b098
heloisaangh <91680274+heloisaangh@users.noreply.github.com>
2022-04-26 09:29:13 -0300

    Create usuarioForm.php

---
+++ dentro de página/usuarioForm.php
@@ -0,0 +1,42 @@
+<?php
+include "../database/db.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <br>
+    <?php
+    $objBD = new BD();
+    $objBD->conn(); //abre a conexão
+
+    if (!empty($_GET['id'])) {
+        echo "Salvar<br>";
+        var_dump($_POST);
+        $objBD->inserir($_POST);
+        header("Location:index.php");
+    }
+
+    ?>
+
+<h2>Formulário Cliente</h2>
+    <form action="index.php" method="post">
+        <label for="nome">Nome:</label></br>
+        <input type="text" id="id" name="nome"></br>
+        <label>Telefone:</label></br>
+        <input type="text" id="id1" name="telefone" placeholder="(49)99999-9999"></br>
+        <lablel for="cpf">CPF:</lablel></br>
+        <input type="text" id="id2" name="cpf" placeholder="000.000.000.00"><br></br>
+        <input type="submit" value="Salvar">
+    </form>
+    <a href="./usuarioList.php">Voltar</a>
+</body>
+
+</html>
